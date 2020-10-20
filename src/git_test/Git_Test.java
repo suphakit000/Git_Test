946a5a58e8919f194b05719c20d08ba18b646c82
tyler <suphakit.tob@mail.kmutt.ac.th>
2020-10-20 18:00:50 +0700

    Add new feature

--- src/git_test/Git_Test.java
+++ src/git_test/Git_Test.java
@@ -16,9 +16,14 @@ public class Git_Test {
      */
     public static void main(String[] args) {
         sayHello();
+        sayGoodBye();
     }
     
     public static void sayHello(){
         System.out.println("hello");
+    }
+    
+    public static void sayGoodBye(){
+        System.out.println("GoodBye");
     }
 }
